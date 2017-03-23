f1f097951c567b9a211a0ce4e378d24b675857e2
Michael Finch <mfinch@squareup.com>
2017-03-23 09:46:01 -0700

    PR feedback

--- job-runner/client/http.go
+++ job-runner/client/http.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strconv"
 
 	"github.com/square/spincycle/proto"
 )
@@ -29,8 +28,8 @@ type jrClient struct {
 	baseUrl string
 }
 
-// NewJRClient takes an http.Client and base API URL and creates a new jrClient.
-func NewJRClient(c *http.Client, baseUrl string) *jrClient {
+// NewJRClient takes an http.Client and base API URL and creates a JRClient.
+func NewJRClient(c *http.Client, baseUrl string) JRClient {
 	return &jrClient{
 		Client:  c,
 		baseUrl: baseUrl,
@@ -39,29 +38,21 @@ func NewJRClient(c *http.Client, baseUrl string) *jrClient {
 
 func (c *jrClient) NewJobChain(jobChain proto.JobChain) error {
 	// POST /api/v1/job-chains
-	httpMethod := "POST"
 	url := c.baseUrl + "/api/v1/job-chains"
-	successStatusResp := 200
 
 	// Create the payload.
-	payloadBytes, err := json.Marshal(jobChain)
+	payload, err := json.Marshal(jobChain)
 	if err != nil {
 		return err
 	}
 
-	// Create the request.
-	req, err := http.NewRequest(httpMethod, url, bytes.NewBuffer(payloadBytes))
+	// Make the request.
+	resp, body, err := c.post(url, payload)
 	if err != nil {
 		return err
 	}
 
-	// Send the request.
-	resp, body, err := c.do(req)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != successStatusResp {
+	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("unsuccessful status code: %d (response body: %s)",
 			resp.StatusCode, string(body))
 	}
@@ -71,23 +62,15 @@ func (c *jrClient) NewJobChain(jobChain proto.JobChain) error {
 
 func (c *jrClient) StartRequest(requestId uint) error {
 	// PUT /api/v1/job-chains/${requestId}/start
-	httpMethod := "PUT"
-	url := c.baseUrl + "/api/v1/job-chains/" + strconv.FormatUint(uint64(requestId), 10) + "/start"
-	successStatusResp := 200
+	url := fmt.Sprintf(c.baseUrl+"/api/v1/job-chains/%d/start", requestId)
 
-	// Create the request.
-	req, err := http.NewRequest(httpMethod, url, nil)
+	// Make the request.
+	resp, body, err := c.put(url)
 	if err != nil {
 		return err
 	}
 
-	// Send the request.
-	resp, body, err := c.do(req)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != successStatusResp {
+	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("unsuccessful status code: %d (response body: %s)",
 			resp.StatusCode, string(body))
 	}
@@ -97,23 +80,15 @@ func (c *jrClient) StartRequest(requestId uint) error {
 
 func (c *jrClient) StopRequest(requestId uint) error {
 	// PUT /api/v1/job-chains/${requestId}/stop
-	httpMethod := "PUT"
-	url := c.baseUrl + "/api/v1/job-chains/" + strconv.FormatUint(uint64(requestId), 10) + "/stop"
-	successStatusResp := 200
+	url := fmt.Sprintf(c.baseUrl+"/api/v1/job-chains/%d/stop", requestId)
 
-	// Create the request.
-	req, err := http.NewRequest(httpMethod, url, nil)
+	// Make the request.
+	resp, body, err := c.put(url)
 	if err != nil {
 		return err
 	}
 
-	// Send the request.
-	resp, body, err := c.do(req)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != successStatusResp {
+	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("unsuccessful status code: %d (response body: %s)",
 			resp.StatusCode, string(body))
 	}
@@ -122,23 +97,15 @@ func (c *jrClient) StopRequest(requestId uint) error {
 
 func (c *jrClient) RequestStatus(requestId uint) (*proto.JobChainStatus, error) {
 	// GET /api/v1/job-chains/${requestId}/status
-	httpMethod := "GET"
-	url := c.baseUrl + "/api/v1/job-chains/" + strconv.FormatUint(uint64(requestId), 10) + "/status"
-	successStatusResp := 200
+	url := fmt.Sprintf(c.baseUrl+"/api/v1/job-chains/%d/status", requestId)
 
-	// Create the request.
-	req, err := http.NewRequest(httpMethod, url, nil)
+	// Make the request.
+	resp, body, err := c.get(url)
 	if err != nil {
 		return nil, err
 	}
 
-	// Send the request.
-	resp, body, err := c.do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != successStatusResp {
+	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("unsuccessful status code: %d (response body: %s)",
 			resp.StatusCode, string(body))
 	}
@@ -155,15 +122,65 @@ func (c *jrClient) RequestStatus(requestId uint) (*proto.JobChainStatus, error) 
 
 // ------------------------------------------------------------------------- //
 
+func (c *jrClient) get(url string) (*http.Response, []byte, error) {
+	// Create the request.
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Send the request.
+	resp, body, err := c.do(req)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return resp, body, nil
+}
+
+func (c *jrClient) put(url string) (*http.Response, []byte, error) {
+	// Create the request.
+	req, err := http.NewRequest("PUT", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Send the request.
+	resp, body, err := c.do(req)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return resp, body, nil
+}
+
+func (c *jrClient) post(url string, payload []byte) (*http.Response, []byte, error) {
+	// Create the request.
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Send the request.
+	resp, body, err := c.do(req)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return resp, body, nil
+}
+
 func (c *jrClient) do(req *http.Request) (*http.Response, []byte, error) {
 	resp, err := c.Client.Do(req)
 	if err != nil {
 		return nil, nil, fmt.Errorf("http.Client.Do: %s", err)
 	}
 	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return resp, nil, fmt.Errorf("ioutil.ReadAll: %s", err)
 	}
+
 	return resp, body, nil
 }
