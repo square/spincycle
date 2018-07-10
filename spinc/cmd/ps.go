b387e488e2f241be1f9156691d125b704e97039e
Lauren Kim <lkim@squareup.com>
2018-07-10 14:00:04 -0700

    ps request name

--- spinc/cmd/ps.go
+++ spinc/cmd/ps.go
@@ -45,15 +45,17 @@ func (c *Ps) Run() error {
 
 	now := time.Now()
 
-	hdr := fmt.Sprintf("%%-20s  %%4s  %%5s  %%6s  %%s\n")
-	line := fmt.Sprintf("%%-20s  %%4d  %%5d  %%6s  %%s\n")
-	fmt.Fprintf(c.ctx.Out, hdr, "ID", "N", "NJOBS", "TIME", "JOB")
+	hdr := fmt.Sprintf("%%-20s  %%4s  %%5s  %%6s  %%s  %%s\n")
+	line := fmt.Sprintf("%%-20s  %%4d  %%5d  %%6s  %%s  %%s\n")
+	fmt.Fprintf(c.ctx.Out, hdr, "ID", "N", "NJOBS", "TIME", "JOB", "REQUEST")
 	for _, r := range status.Jobs {
 		runtime := fmt.Sprintf("%.1f", now.Sub(time.Unix(0, r.StartedAt)).Seconds())
 		nJobs := 0
+		requestName := ""
 		if status.Requests != nil {
 			if r, ok := status.Requests[r.RequestId]; ok {
 				nJobs = r.TotalJobs
+				requestName = r.Type
 			}
 		}
 		jobNameLen := len(r.Name)
@@ -61,7 +63,7 @@ func (c *Ps) Run() error {
 			// "very_long_job_name" -> "very_long_job_..."
 			r.Name = r.Name[jobNameLen-(JOB_COL_LEN-3):jobNameLen] + "..." // -3 for "..."
 		}
-		fmt.Fprintf(c.ctx.Out, line, r.RequestId, r.N, nJobs, runtime, r.Name)
+		fmt.Fprintf(c.ctx.Out, line, r.RequestId, r.N, nJobs, runtime, r.Name, requestName)
 	}
 
 	return nil
