5bacc8e9f304136e4b6a9996449bc35689e4a4a7
Daniel Nichter <daniel-nichter@users.noreply.github.com>
2018-11-07 11:34:47 -0500

    Fix job_log table: sequence_name needs to be nullable until we send/store this value

--- request-manager/resources/request_manager_schema.sql
+++ request-manager/resources/request_manager_schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS `requests` (
-  `request_id`     BINARY(20) NOT NULL,
-  `type`           VARBINARY(75) NOT NULL,
+  `request_id`     BINARY(20)       NOT NULL,
+  `type`           VARBINARY(75)    NOT NULL,
   `state`          TINYINT UNSIGNED NOT NULL DEFAULT 0,
-  `user`           VARCHAR(100) NULL DEFAULT NULL,
-  `created_at`     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `started_at`     TIMESTAMP NULL DEFAULT NULL,
-  `finished_at`    TIMESTAMP NULL DEFAULT NULL,
-  `total_jobs`     INT UNSIGNED NOT NULL DEFAULT 0,
-  `finished_jobs`  INT UNSIGNED NOT NULL DEFAULT 0,
+  `user`           VARCHAR(100)         NULL DEFAULT NULL,
+  `created_at`     TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `started_at`     TIMESTAMP            NULL DEFAULT NULL,
+  `finished_at`    TIMESTAMP            NULL DEFAULT NULL,
+  `total_jobs`     INT UNSIGNED     NOT NULL DEFAULT 0,
+  `finished_jobs`  INT UNSIGNED     NOT NULL DEFAULT 0,
 
   PRIMARY KEY (`request_id`),
   INDEX (`created_at`),  -- recently created
@@ -17,27 +17,27 @@ CREATE TABLE IF NOT EXISTS `requests` (
 
 CREATE TABLE IF NOT EXISTS `raw_requests` (
   `request_id` BINARY(20) NOT NULL,
-  `request`    BLOB NOT NULL,
-  `job_chain`  LONGBLOB NOT NULL,
+  `request`    BLOB       NOT NULL,
+  `job_chain`  LONGBLOB   NOT NULL,
 
   PRIMARY KEY (`request_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `job_log` (
-  `request_id`    BINARY(20) NOT NULL,
-  `job_id`        BINARY(4) NOT NULL,
-  `name`          VARBINARY(100) NOT NULL,
-  `try`           SMALLINT NOT NULL DEFAULT 0,
-  `sequence_try`  SMALLINT NOT NULL DEFAULT 0,
-  `sequence_name` VARBINARY(100) NOT NULL,
-  `type`          VARBINARY(75) NOT NULL,
+  `request_id`    BINARY(20)       NOT NULL,
+  `job_id`        BINARY(4)        NOT NULL,
+  `name`          VARBINARY(100)   NOT NULL,
+  `try`           SMALLINT         NOT NULL DEFAULT 0,
+  `sequence_try`  SMALLINT         NOT NULL DEFAULT 0,
+  `sequence_name` VARBINARY(100)       NULL DEFAULT NULL,
+  `type`          VARBINARY(75)    NOT NULL,
   `state`         TINYINT UNSIGNED NOT NULL DEFAULT 0,
-  `started_at`    BIGINT UNSIGNED NOT NULL DEFAULT 0, -- Unix time (nanoseconds)
-  `finished_at`   BIGINT UNSIGNED NOT NULL DEFAULT 0, -- Unix time (nanoseconds)
-  `error`         TEXT NULL DEFAULT NULL,
-  `exit`          TINYINT UNSIGNED NULL DEFAULT NULL,
-  `stdout`        LONGBLOB NULL DEFAULT NULL,
-  `stderr`        LONGBLOB NULL DEFAULT NULL,
+  `started_at`    BIGINT UNSIGNED  NOT NULL DEFAULT 0, -- Unix time (nanoseconds)
+  `finished_at`   BIGINT UNSIGNED  NOT NULL DEFAULT 0, -- Unix time (nanoseconds)
+  `error`         TEXT                 NULL DEFAULT NULL,
+  `exit`          TINYINT UNSIGNED     NULL DEFAULT NULL,
+  `stdout`        LONGBLOB             NULL DEFAULT NULL,
+  `stderr`        LONGBLOB             NULL DEFAULT NULL,
 
   PRIMARY KEY (`request_id`, `job_id`, `try`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
